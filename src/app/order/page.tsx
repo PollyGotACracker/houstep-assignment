87c58f9a8d210567798ec9395a95b0b6873436e4
PollyGotACracker <bitterns96@gmail.com>
2024-01-19 02:23:26 +0900

    feat: ProductItem 레이아웃 구현

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,12 +1,35 @@
+import styles from "./page.module.css";
 import Header from "./_component/Header";
 import UserOrder from "./_component/UserOrder";
 import Loader from "./_component/Loader";
+import ProductItem from "./_component/ProductItem";
 
 export default function OrderPage() {
   return (
     <main>
       <Header />
-      <Loader />
+      {/* <Loader /> */}
+      <ul className={styles.product_list}>
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+        <ProductItem />
+      </ul>
       <UserOrder />
     </main>
   );
