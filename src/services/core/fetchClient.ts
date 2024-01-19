f340f6f1407547ec1dc25442bfdf10a4e577784d
PollyGotACracker <bitterns96@gmail.com>
2024-01-20 01:30:21 +0900

    fix: API base url 변경

--- src/services/core/fetchClient.ts
+++ src/services/core/fetchClient.ts
@@ -1,5 +1,7 @@
 const fetchClient = async (endPoint: string, options: RequestInit) => {
-  return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${endPoint}`, {
+  const BASE_URL =
+    process.env.NODE_ENV === "production" ? "/" : "http://localhost:3001";
+  return fetch(`${BASE_URL}${endPoint}`, {
     ...options,
     headers: {
       ...options.headers,
