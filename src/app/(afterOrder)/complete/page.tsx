c2ad4540e7d77c0864a7007058421c5afa5dde1a
PollyGotACracker <bitterns96@gmail.com>
2024-01-19 21:37:49 +0900

    feat: Order 주문 성공 시 장바구니 초기화

--- src/app/(afterOrder)/complete/page.tsx
+++ src/app/(afterOrder)/complete/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 
 import Image from "next/image";
+import { useEffect } from "react";
+import { useResetRecoilState } from "recoil";
+import { orderCartState } from "@/atoms/order";
 import CenteredLayout from "@/app/_component/CenteredLayout";
 
 export default function CompletePage() {
+  const resetCart = useResetRecoilState(orderCartState);
+
+  useEffect(() => resetCart(), []);
+
   return (
     <CenteredLayout gap="12px">
       <Image
