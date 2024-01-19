79c29a4fca47a7bc8e871e79201de27df03ca8c3
PollyGotACracker <bitterns96@gmail.com>
2024-01-19 13:29:35 +0900

    feat: Order items 가져오기 구현

---
+++ src/services/order.ts
@@ -0,0 +1,12 @@
+import fetchClient from "./fetchClient";
+
+export const getOrderItems = async () => {
+  const res = await fetchClient(`/items`, {
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (!res.ok) {
+    throw new Error("목록을 불러오는 중 문제가 발생했습니다.");
+  }
+  return res.json();
+};
---
+++ src/types/order.ts
@@ -0,0 +1,7 @@
+export interface OrderItemType {
+  id: string;
+  name: string;
+  event: number;
+  materialType: number;
+  price: number;
+}
